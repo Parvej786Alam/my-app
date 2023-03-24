d91d19f05c4a9ef76db710239d3efb707e9a29c5
Parvej <parvej.alam624@gmail.com>
2023-03-25 04:27:21 +0530

    Initial commit

---
+++ src/components/About.js
@@ -0,0 +1,81 @@
+import React from 'react'
+
+export default function About(props) {
+
+    // const [myStyle, setMyStyle] = useState({
+    //     color: 'black',
+    //     backgroundColor: 'white'
+    // });
+
+    let myStyle = {
+        color: props.mode === 'dark' ? 'white' : '#042743',
+        backgroundColor: props.mode === 'dark' ? 'rgb(36 74 104)' : 'white',
+    }
+
+    // const [btntext, setBtnText] = useState("Enable Dark mode");
+
+    // const toggleStyle = () => {
+    //     if (myStyle.color === 'black') {
+    //         setMyStyle({
+    //             color: 'white',
+    //             backgroundColor: 'black',
+    //             border: '1px solid white'
+    //         })
+    //         setBtnText("Enable Light Mode");
+    //     }
+    //     else {
+    //         setMyStyle({
+    //             color: 'black',
+    //             backgroundColor: 'white'
+    //         })
+    //         setBtnText("Enable Dark Mode");
+    //         style={{ color: props.mode === 'dark' ? 'white' : '#42743' }}
+    //     }
+
+    return (
+        <div className="container">
+            <h1 className="my-3"style={myStyle} >About Us</h1>
+            <div className="accordion" id="accordionExample"style={myStyle} >
+                <div className="accordion-item"style={myStyle} >
+                    <h2 className="accordion-header" id="headingOne">
+                        <button className="accordion-button" type="button" style={myStyle} data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            <strong>Analyze your text</strong>
+                        </button>
+                    </h2>
+                    <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                        <div className="accordion-body" style={myStyle}>
+                            TextUtlis give you a way to analyze youe text quickly and efficiently. Be it word count, character count or
+                        </div>
+                    </div>
+                </div>
+                <div className="accordion-item" style={myStyle}>
+                    <h2 className="accordion-header" id="headingTwo">
+                        <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            <strong>Free to use</strong>
+                        </button>
+                    </h2>
+                    <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                        <div className="accordion-body" style={myStyle}>
+                            Textutlis is a free character counter tool that provides instant character count and word count statistics for a given text. Textutlis report the numberof word and character. This is it suitable for writing text with word/charater limit.
+                        </div>
+                    </div>
+                </div>
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingThree">
+                        <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            <strong>Browser compatible</strong>
+                        </button>
+                    </h2>
+                    <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                        <div className="accordion-body" style={myStyle}>
+                            This counter software work in any web browser such as Chrome, Firefox, Internet Explorer, Safari, Opera. It suits to count character in facebook, blog, book, exal document, pdf document, essasy, etc.
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* <div className="container my-3">
+                <button onClick={toggleStyle} type="button" className="btn btn-primary">{btntext}</button>
+            </div> */}
+        </div>
+    );
+}
